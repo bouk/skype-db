16d0921b2f4fb5f4ca33490283c14c26322618fa
Bouke van der Bijl <boukevanderbijl@gmail.com>
2013-04-04 06:51:00 +0200

    Add default scope to message to only get actual messages

--- lib/skype_db/models/message.rb
+++ lib/skype_db/models/message.rb
@@ -5,6 +5,9 @@ module SkypeDB
 
     belongs_to :contact, foreign_key: :author
 
+    # type 61 are chat messages instead of friend requests etc.
+    default_scope where(type: 61)
+
     def body
       '' if body_xml.empty?
       body_xml.gsub(/<.*?>/, '')
