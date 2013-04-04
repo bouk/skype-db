d0199d845b541bc0b45138bab904d8c37b381a28
Bouke van der Bijl <boukevanderbijl@gmail.com>
2013-04-04 05:27:57 +0200

    Order contacts by skypename by default

--- lib/skype_db/models/contact.rb
+++ lib/skype_db/models/contact.rb
@@ -2,6 +2,7 @@ module SkypeDB
   class Contact < ActiveRecord::Base
     self.table_name = 'Contacts'
     self.inheritance_column = 'object_type'
+    default_scope order('skypename')
 
     has_many :messages, primary_key: :skypename, foreign_key: :author
   end
