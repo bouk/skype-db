718b0d84dbe81206478b436534aa13f895330567
Bouke van der Bijl <boukevanderbijl@gmail.com>
2013-04-04 05:23:19 +0200

    Add associations between Contact and Message

--- lib/skype_db/models/contact.rb
+++ lib/skype_db/models/contact.rb
@@ -2,5 +2,7 @@ module SkypeDB
   class Contact < ActiveRecord::Base
     self.table_name = 'Contacts'
     self.inheritance_column = 'object_type'
+
+    has_many :messages, primary_key: :skypename, foreign_key: :author
   end
 end
--- lib/skype_db/models/message.rb
+++ lib/skype_db/models/message.rb
@@ -2,5 +2,7 @@ module SkypeDB
   class Message < ActiveRecord::Base
     self.table_name = 'Messages'
     self.inheritance_column = 'object_type'
+
+    belongs_to :contact, primary_key: :skypename, foreign_key: :author
   end
 end
